ff58a26fb47a0a1ce277a697425938fc1e05ea60
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-03-03 18:39:20 +0530

    Rename quick-onboard to onboarding, since the flowname is hardcoded in many places across the front-end and back-end

--- test/e2e/specs/wp-signup-spec.js
+++ test/e2e/specs/wp-signup-spec.js
@@ -1278,7 +1278,6 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 				driver,
 				StartPage.getStartURL( {
 					culture: locale,
-					flow: 'onboarding',
 					query: 'vertical=art',
 				} )
 			);
