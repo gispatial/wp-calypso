e0b499209c4d747539980e1428b85cb7ad1dce0d
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-03-03 19:02:29 +0530

    Link to the master thread in flow description.

--- test/e2e/specs/wp-signup-spec.js
+++ test/e2e/specs/wp-signup-spec.js
@@ -1278,6 +1278,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 				driver,
 				StartPage.getStartURL( {
 					culture: locale,
+					flow: 'onboarding-with-preview',
 					query: 'vertical=art',
 				} )
 			);
