87847845d3846e9133f5628006abdcdb11daf7b5
Rich Collier <rcollier@iris.local>
2020-03-03 11:33:19 -0500

    Jetpack Cloud: Daily backup status component

--- client/landing/jetpack-cloud/sections/backups/main.jsx
+++ client/landing/jetpack-cloud/sections/backups/main.jsx
@@ -10,12 +10,30 @@ import { connect } from 'react-redux';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { requestActivityLogs } from 'state/data-getters';
 import ActivityList from '../../components/activity-list';
+import DatePicker from '../../components/date-picker';
+import DailyBackupStatus from '../../components/daily-backup-status';
+import { getBackupAttemptsForDate } from './utils';
 
 class BackupsPage extends Component {
+	state = {
+		currentDateSetting: false,
+	};
+
+	dateChange = currentDateSetting => this.setState( { currentDateSetting } );
+
 	render() {
+		const { logs, siteId } = this.props;
+		const initialDate = new Date();
+		const currentDateSetting = this.state.currentDateSetting
+			? this.state.currentDateSetting
+			: new Date().toISOString().split( 'T' )[ 0 ];
+
+		const backupAttempts = getBackupAttemptsForDate( logs, currentDateSetting );
+
 		return (
 			<div>
-				<p>Welcome to the backup detail page for site { this.props.siteId }</p>
+				<DatePicker siteId={ siteId } initialDate={ initialDate } onChange={ this.dateChange } />
+				<DailyBackupStatus date={ currentDateSetting } backupAttempts={ backupAttempts } />
 				<ActivityList logs={ this.props.logs } />
 			</div>
 		);
@@ -24,10 +42,10 @@ class BackupsPage extends Component {
 
 export default connect( state => {
 	const siteId = getSelectedSiteId( state );
-	const logs = requestActivityLogs( siteId, { group: 'rewind' } );
+	const logs = siteId && requestActivityLogs( siteId, { group: 'rewind' } );
 
 	return {
 		siteId,
-		logs,
+		logs: logs?.data ?? [],
 	};
 } )( BackupsPage );
